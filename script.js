59f24fb081b94d5d0c0e375100cefee8c154ad6d
ErminS1 <erminklein@gmail.com>
2022-10-11 13:57:14 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+alert("goeie dag allemaal");
+document.getElementById("title").style.color = "red"
+
+var getal1 = 12;
+var getal2 = 4;
+var som = getal1 * getal2;
+alert(getal1 + "X" + getal2 + "=" + som);
+
+var kleur = prompt("geef een engelse kleurnaam:", "");
+document.getElementById("title").style.color =kleur;
+
+document.getElementById("p1").innerHTML = "Hello"+ kleur +"!";
